9da1c7e72e6b62e62d557e912874a7e129d915aa
Abdul Karim <Abdul.moro4u@gmail.com>
2022-01-27 00:03:12 +0900

    Receiving and sending message to the client

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,30 @@ fn main() {
     loop {
         if let Ok((mut Socket , addr ))=server.accept(){
 
-            println!("client connected {}",addr);
+            println!("client{}connected ",addr);
+            let tx = tx.clone();
+            clients.push(socket.try_clone().expect("failed to clone client"));
+
+            thread::spawn(move || loop {
+
+                let mut buff = vec![0;MSG_SIZE];
+                match socket.read_exact(&mut buff){
+                    Ok(_) => {
+                        let msg = buff.into_iter().take_while(|&x|x!=0).collect::<Vec<_>>();
+                        let msg = String::from_utf8(msg).expect("invalid utf8 message");
+                        println!("{}: {:?}",addr,msg);
+                        tx.send(msg).expect("failed to send the message rx");
+                    },
+                    Err(ref err)if err.kind() == ErrorKind::WouldBlock =>(),
+                    Err(_)=>{
+                        println!("Closing connection with {}",addr);
+                        break;
+                    }
+
+
+
+                }
+            })
         }
     }
 
